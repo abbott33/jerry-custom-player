28f09abd1afb3d8e4db8fc56d818aa098aa467d6
abbott <abbott@abiechina.com>
2021-08-30 10:47:29 +0800

    init project

---
+++ player-api-manage/src/main/java/com/player/PlayerMain.java
@@ -0,0 +1,15 @@
+package com.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Abbott
+ * @date 2021/8/27
+ */
+@SpringBootApplication
+public class PlayerMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PlayerMain.class, args);
+    }
+}
